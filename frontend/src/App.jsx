83642d78c2f6febfe5621887f0aab83e731d2f17
jang2714 <prkq6336@naver.com>
2025-01-27 19:52:52 +0900

    ch1-5. 하단바

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import Footer from "../component/Footer/Footer";
 import Navbar from "../component/Navbar/Navbar";
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
@@ -6,6 +7,8 @@ function App() {
   return (
     <BrowserRouter>
       <Navbar/>
+      
+      <Footer/>
     </BrowserRouter>
   );
   
