f1df2411a996149daccb1ffb2d43c04fbb69a7fc
jang2714 <prkq6336@naver.com>
2025-01-28 16:06:37 +0900

    ch2-1 MainPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,60 @@
 import Footer from "../component/Footer/Footer";
 import Navbar from "../component/Navbar/Navbar";
+import About from "../page/About/About";
+import Board from "../page/Board/Board";
+import Contact from "../page/Contact/Contact";
+import Leadership from "../page/Leadership/Leadership";
+import MainPage from "../page/MainPage/MainPage";
+import Services from "../page/Services/Services";
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+
+function Layout() {
+  return (
+    <>
+    <Navbar />
+    <Outlet />
+    <Footer />
+    </>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <MainPage />
+      },
+      {
+        path: "/about",
+        element: <About />
+      },
+      {
+        path: "/leadership",
+        element: <Leadership />
+      },
+      {
+        path: "/board",
+        element: <Board />
+      },
+      {
+        path: "/our-services",
+        element: <Services />
+      },
+      {
+        path: "/contact",
+        element: <Contact />
+      }
+    ]
+  }
+])
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Navbar/>
-      
-      <Footer/>
-    </BrowserRouter>
-  );
-  
+  return <RouterProvider router={router} />
 }
 
 export default App;
